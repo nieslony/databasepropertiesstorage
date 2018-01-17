2c48a47729e3ea7e082ac2954c173636e2714eab
Claas Nieslony <bb@nieslony.at>
2018-01-17 21:48:11 +0100

    Fix: constraints

--- src/sql/create-properties-storage.sql
+++ src/sql/create-properties-storage.sql
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -18,20 +18,20 @@ CREATE INDEX ON propertygroups (name);;
 
 CREATE TABLE properties (
     group_id SERIAL references propertyGroups(id),
-    name varchar NOT NULL UNIQUE,
+    name varchar NOT NULL,
     value varchar,
     UNIQUE (group_id, name)
 );;
 
 
-CREATE OR REPLACE FUNCTION set_property(_group_id int, _name varchar, _value varchar) RETURNS VOID AS $$ 
-    DECLARE 
-    BEGIN 
+CREATE OR REPLACE FUNCTION set_property(_group_id int, _name varchar, _value varchar) RETURNS VOID AS $$
+    DECLARE
+    BEGIN
         UPDATE properties SET value = _value WHERE group_id = _group_id AND name = _name;
-        IF NOT FOUND THEN 
-            INSERT INTO properties values(_group_id, _name, _value); 
-        END IF; 
-    END; 
+        IF NOT FOUND THEN
+            INSERT INTO properties values(_group_id, _name, _value);
+        END IF;
+    END;
     $$ LANGUAGE 'plpgsql';;
 
 
